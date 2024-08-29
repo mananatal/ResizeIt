7dafd0c28a2f6ea143b1c2a79286543afbf0c468
mananatal <mananatal25@gmail.com>
2024-08-29 19:34:53 +0530

    Add upload-videos route

--- app/api/upload-videos/route.ts
+++ app/api/upload-videos/route.ts
@@ -1,7 +1,10 @@
 import { auth } from "@clerk/nextjs/server";
+import { PrismaClient } from "@prisma/client";
 import { v2 as cloudinary } from 'cloudinary';
 import { NextRequest, NextResponse } from "next/server";
 
+
+const prisma = new PrismaClient();
 
 cloudinary.config({
     cloud_name: process.env.NEXT_PUBLIC_CLOUD_NAME,
@@ -12,6 +15,8 @@ cloudinary.config({
 
 interface CloudinaryUploadResult {
     public_id: string;
+    bytes: number;
+    duration?: number;
     [key: string]: any
 }
 
@@ -47,9 +52,12 @@ export async function POST(request:NextRequest){
         }
 
         const formData=await request.formData();
-        const file=formData.get("file") as File | null ;
+        const videoFile=formData.get("video") as File | null ;
+        const title = formData.get("title") as string;
+        const description = formData.get("description") as string;
+        const originalSize = formData.get("originalSize") as string;
 
-        if(!file){
+        if(!videoFile){
             return NextResponse.json(
                 {
                     error: "File not found"
@@ -60,13 +68,15 @@ export async function POST(request:NextRequest){
             );
         }
 
-        const bytes=await file.arrayBuffer();
+        const bytes=await videoFile.arrayBuffer();
         const buffer=Buffer.from(bytes);
 
         const uploadResult = await new Promise<CloudinaryUploadResult>((resolve,reject) => {
             cloudinary.uploader.upload_stream(
                 {
-                    folder:process.env.CLOUDINARY_IMAGE_FOLDER
+                    folder:process.env.CLOUDINARY_VIDEO_FOLDER,
+                    allowed_formats:["mp4"],
+                    resource_type:"video"                    
                 },
                 (error, result) => {
                     if(error){
@@ -74,14 +84,25 @@ export async function POST(request:NextRequest){
                     }
                     else{
                         resolve(result as CloudinaryUploadResult);
-                    } 
-                    
-            }).end(buffer);
+                    }                    
+                }
+            ).end(buffer);
+        });
+
+        const video=await prisma.video.create({
+            data:{
+                title,
+                description,
+                originalSize,
+                compressedSize:String(uploadResult.bytes),
+                duration:uploadResult.duration || 0,               
+                publicId:uploadResult.public_id
+            }
         });
 
         return NextResponse.json(
             {
-                publicId: uploadResult.public_id
+                video
             },
             {
                 status: 200
@@ -93,7 +114,7 @@ export async function POST(request:NextRequest){
         console.log("UPload image failed", error)
         return NextResponse.json(
             {
-                error: "Upload image failed"            
+                error: "Upload Video failed"            
             }, 
             {
                 status: 500
@@ -101,5 +122,7 @@ export async function POST(request:NextRequest){
         );
 
     }
-    
+    finally{
+        await prisma.$disconnect();
+    }
 }
